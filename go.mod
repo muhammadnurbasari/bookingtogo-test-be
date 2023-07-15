2fccb2fda01df4898b1fca8c80c129b0169f2d7a
muhammadnurbasari <m.nurbasari@gmail.com>
2023-07-15 22:46:50 +0700

    connection db mySQL

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module bookingtogo-test-be
 
 go 1.18
+
+require github.com/rs/zerolog v1.23.0
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-sql-driver/mysql v1.7.1 
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"bookingtogo-test-be/connectDB"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Info().Msg("Read file env. . .")
+	err := godotenv.Load("config/.env")
+	if err != nil {
+		log.Error().Msg(err.Error())
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	conn, errConn := connectDB.ConnMySQL(dbHost, dbPort, dbUser, dbPass, dbName)
+
+	if errConn != nil {
+		log.Error().Msg(errConn.Error())
+	}
+	fmt.Println(conn)
 }
